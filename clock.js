fd6df537e4dc9c582b433103ac3becf9e1cc14fc
Brian Monge <bm0801389031@gmail.com>
2021-01-13 17:46:05 -0700

    clock program

---
+++ clock.js
@@ -0,0 +1,6 @@
+/*Clock shows h hours, m minutes and s seconds after
+ midnight. Your task is to write a function which returns
+ the time since midnight in milliseconds.*/
+function past(h, m, s){
+// code here
+}
