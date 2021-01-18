065ecb19a5700734734e4e62892b774e30aa04a9
Brian Monge <bm0801389031@gmail.com>
2021-01-18 11:22:21 -0700

    added new file opposite number

---
+++ opposite number.js
@@ -0,0 +1,30 @@
+/*
+Description:
+
+Very simple, given a number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+*/
+
+function opposite(number) {
+
+}
+
+
+
+
+
+
+
+
+// test cases
+Test.assertEquals(opposite(1), -1,)
+Test.assertEquals(opposite(0), 0,)
+Test.assertEquals(opposite(4.25), -4.25,)
+Test.assertEquals(opposite(3.3333333), -3.3333333,)
+Test.assertEquals(opposite(-12525220.3325), 12525220.3325,)
+Test.assertEquals(opposite(-5), 5,)
